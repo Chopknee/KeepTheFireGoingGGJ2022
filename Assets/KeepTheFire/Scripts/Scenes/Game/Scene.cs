a57bbbd5fedf0c06fa1e93606b16d65195a83149
Edwin S <edwinsanchez2003@gmail.com>
2022-01-22 13:31:27 -0500

    Added deer

--- Assets/KeepTheFire/Scripts/Scenes/Game/Scene.cs
+++ Assets/KeepTheFire/Scripts/Scenes/Game/Scene.cs
@@ -7,6 +7,8 @@ namespace KeepTheFire.Scenes.Game {
 
 		private new UnityEngine.Camera camera = null;
 
+		private GameObject pool = null;
+		private Deer[] deers = null;
 
 		private void Awake() {
 
@@ -14,8 +16,18 @@ namespace KeepTheFire.Scenes.Game {
 
 			camera = transform.Find("Camera").GetComponent<UnityEngine.Camera>();
 			camera.gameObject.AddComponent<Game.Camera>();
-			
-			
+
+			pool = new GameObject("Pool");
+			pool.transform.SetParent(transform);
+			pool.transform.localPosition = new Vector3(0, -100, 0);
+			GameObject deerPrefab = Resources.Load<GameObject>("KeepTheFire/Scenes/Game/Animals/Deer");
+			deers = new Deer[5];
+
+			for(int i = 0; i < deers.Length; i++) {
+				deers[i] = Instantiate(deerPrefab).AddComponent<Deer>();
+				deers[i].transform.SetParent(pool.transform);
+				deers[i].transform.localPosition = Vector3.zero;
+            }
 		}
 
 		private void Update() {
