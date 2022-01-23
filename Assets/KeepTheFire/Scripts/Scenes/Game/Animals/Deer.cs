f9b7e8e32d3ae9c1ce665707e2a2b6eab033ec3c
Brian <chopknee@gmail.com>
2022-01-22 22:26:26 -0500

    IDK

--- Assets/KeepTheFire/Scripts/Scenes/Game/Animals/Deer.cs
+++ Assets/KeepTheFire/Scripts/Scenes/Game/Animals/Deer.cs
@@ -9,21 +9,18 @@ namespace KeepTheFire.Scenes.Game
         private Dugan.UI.Button button = null;
 
         public int state = 0;
-        public float walkSpeed = 1.7f;
-        public float startleSpeed = 2.7f;
 
         // starting elevation
         public float elevation = 3.0f;
 
         // how far left the deers start
-        public float startX = -10.0f;
+        public float startOffsetX = -16.0f;
+        public float startOffsetZ = 0f;
+        public float offsetZ = 0f;
+        public float bounds = 24.0f;
 
-        // how far right the deers travel
-        public float bounds = 10.0f;
-
-        // starting pos and step size for randomized movement
-        public float startZ;
-        public float moveZ;
+        public float xPos;
+        float faceForward;
 
         private Vector3 homePos = new Vector3(0.0f, -90.0f, 0.0f);
 
@@ -39,35 +36,44 @@ namespace KeepTheFire.Scenes.Game
         void Update()
         {
             if(state == 0) {
-                if(Random.Range(0.0f, 1.0f) < 0.001f) {
+                if(Random.Range(0.0f, 1.0f) < 0.005f) {
                     activate();
                     state++;
                 }
             }
             else if(state == 1) {
                 // do movement
-                movement(walkSpeed);
+                movement(1.7f);
                 checkStopCycle();
             }
             else if(state == 2) {
-                movement(-startleSpeed);
+                movement(-2.7f);
                 checkStopCycle();
             }
         }
 
         private void activate() { // puts deer in starting position
-            moveZ = Random.Range(-1.0f, 1.0f);
-            startZ = Random.Range(-5.0f, 6.0f);
-            transform.position = new Vector3(startX, elevation, startZ);
+            startOffsetZ = Random.Range(3.5f, 4.5f);
+            if(Random.Range(-1f,1f) < 0) {
+                startOffsetX = -startOffsetX;
+            }
+            transform.position = new Vector3(startOffsetX, elevation, startOffsetZ);
+            offsetZ = Random.Range(-0.008f, 0.008f);
         }
 
         private void movement(float speed) { // moves the deer across screen
-            // move distance
-            float stepX = speed*Time.deltaTime;
-            float stepZ = moveZ*Time.deltaTime;
-
-            // set position of deer based on step size
-            transform.position = transform.position + new Vector3(stepX, 0f, stepZ);
+            if(startOffsetX < 0) {
+                xPos = transform.position.x + speed*Time.deltaTime;
+                transform.forward = new Vector3(speed, 0f, offsetZ);
+            }
+            else {
+                xPos = transform.position.x - speed*Time.deltaTime;
+                transform.forward = new Vector3(-speed, 0f, offsetZ);
+            }
+            
+            startOffsetZ += offsetZ;
+            transform.position = new Vector3(xPos, elevation, startOffsetZ);
+            
         }
 
         private void deactivate() {
@@ -83,15 +89,23 @@ namespace KeepTheFire.Scenes.Game
             }
         }
 
-        
         private void OnButtonClicked(Dugan.Input.PointerTarget target, string args) {
-            moveZ = -moveZ;
-            if(moveZ < 0) {
-                moveZ = moveZ - Random.Range(1.0f, 3.0f);
+            // check if already flipped
+            if(state != 1) {
+                return;
+            }
+            // add to log pile
+            Scene.instance.logStashe += 0.1f;
+
+            // Adjust for stupid offset when rotating
+            if(startOffsetX < 0) {
+                transform.position = transform.position - new Vector3(6f, 0f, 0f);
             }
             else {
-                moveZ = moveZ + Random.Range(1.0f, 3.0f);
+                transform.position = transform.position + new Vector3(6f, 0f, 0f);
             }
+
+            // move to state 2
             state = 2;
         }
     }
