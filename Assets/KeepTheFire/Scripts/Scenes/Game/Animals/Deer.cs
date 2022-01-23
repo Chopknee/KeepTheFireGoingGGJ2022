bea508ca1cf387bc53205467490898ecfaeeafee
Edwin S <edwinsanchez2003@gmail.com>
2022-01-22 22:24:41 -0500

    "deer movement and click smoothed out"

--- Assets/KeepTheFire/Scripts/Scenes/Game/Animals/Deer.cs
+++ Assets/KeepTheFire/Scripts/Scenes/Game/Animals/Deer.cs
@@ -9,21 +9,18 @@ namespace KeepTheFire.Scenes.Game
         private Dugan.UI.Button button = null;
 
         public int state = 0;
-        public float walkSpeed = 1.7f;
-        public float startleSpeed = 2.7f;
 
         // starting elevation
         public float elevation = 3.0f;
 
         // how far left the deers start
-        public float startX = -10.0f;
+        public float startOffsetX = -16.0f;
+        public float startOffsetZ = 0f;
+        public float offsetZ = 0f;
+        public float bounds = 24.0f;
 
-        // how far right the deers travel
-        public float bounds = 11.0f;
-
-        // starting pos and step size for randomized movement
-        public float startZ;
-        public float moveZ;
+        public float xPos;
+        float faceForward;
 
         private Vector3 homePos = new Vector3(0.0f, -90.0f, 0.0f);
 
@@ -33,8 +30,6 @@ namespace KeepTheFire.Scenes.Game
             // gameObject.SetActive(false);
             button = gameObject.AddComponent<Dugan.UI.Button>();
             button.OnClicked += OnButtonClicked;
-
-            //transform.Rotate(0, 90, 0);
         }
 
         // Update is called once per frame
@@ -48,40 +43,37 @@ namespace KeepTheFire.Scenes.Game
             }
             else if(state == 1) {
                 // do movement
-                movement(walkSpeed);
+                movement(1.7f);
                 checkStopCycle();
             }
             else if(state == 2) {
-                movement(-startleSpeed);
+                movement(-2.7f);
                 checkStopCycle();
             }
         }
 
         private void activate() { // puts deer in starting position
-            moveZ = Random.Range(-1.0f, 1.0f);
-            startZ = Random.Range(-5.0f, 6.0f);
-            
-            if(Random.Range(-1f, 1f) > 0) {
-                startX = -startX;
-                // adjust rotation of deer
-                //transform.Rotate(0, -180, 0);
+            startOffsetZ = Random.Range(3.5f, 4.5f);
+            if(Random.Range(-1f,1f) < 0) {
+                startOffsetX = -startOffsetX;
             }
-
-            transform.position = new Vector3(startX, elevation, startZ);
+            transform.position = new Vector3(startOffsetX, elevation, startOffsetZ);
+            offsetZ = Random.Range(-0.008f, 0.008f);
         }
 
         private void movement(float speed) { // moves the deer across screen
-            // move distance
-            float stepX = speed*Time.deltaTime;
-            float stepZ = moveZ*Time.deltaTime;
-
-            if(startX > 0) {
-                stepX = -stepX;
+            if(startOffsetX < 0) {
+                xPos = transform.position.x + speed*Time.deltaTime;
+                transform.forward = new Vector3(speed, 0f, offsetZ);
             }
-
-            // set position of deer based on step size
-            transform.position = transform.position + new Vector3(stepX, 0f, stepZ);
-            transform.forward = new Vector3(stepX, 0f, stepZ).normalized;
+            else {
+                xPos = transform.position.x - speed*Time.deltaTime;
+                transform.forward = new Vector3(-speed, 0f, offsetZ);
+            }
+            
+            startOffsetZ += offsetZ;
+            transform.position = new Vector3(xPos, elevation, startOffsetZ);
+            
         }
 
         private void deactivate() {
@@ -97,28 +89,23 @@ namespace KeepTheFire.Scenes.Game
             }
         }
 
-        
         private void OnButtonClicked(Dugan.Input.PointerTarget target, string args) {
-            Debug.Log("Deer Clicked!");
+            // check if already flipped
             if(state != 1) {
                 return;
             }
-            
-            // Rotate deer
-            //transform.Rotate(0, 180, 0);
-
-            // add to log
+            // add to log pile
             Scene.instance.logStashe += 0.1f;
 
-            moveZ = -moveZ;
-            
-            if(moveZ < 0) {
-                moveZ = moveZ - Random.Range(1.0f, 3.0f);
+            // Adjust for stupid offset when rotating
+            if(startOffsetX < 0) {
+                transform.position = transform.position - new Vector3(6f, 0f, 0f);
             }
             else {
-                moveZ = moveZ + Random.Range(1.0f, 3.0f);
+                transform.position = transform.position + new Vector3(6f, 0f, 0f);
             }
-            
+
+            // move to state 2
             state = 2;
         }
     }
